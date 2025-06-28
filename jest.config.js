5ec0fbffc41014bb0b89d30458b2755abf542f51
aantunes89 <andrepalhares1989@gmail.com>
2025-06-28 12:30:42 +0100

    chore: adding jest and removing jasmine/karma

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+    globalSetup: 'jest-preset-angular/global-setup',
+    testEnvironment: 'jsdom',
+    moduleNameMapping: {
+        '^src/(.*)$': '<rootDir>/src/$1',
+        '^app/(.*)$': '<rootDir>/src/app/$1',
+        '^assets/(.*)$': '<rootDir>/src/assets/$1',
+        '^environments/(.*)$': '<rootDir>/src/environments/$1',
+    },
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/**/*.spec.ts',
+        '!src/**/*.d.ts',
+        '!src/main.ts',
+        '!src/polyfills.ts',
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov', 'html'],
+    testMatch: [
+        '<rootDir>/src/**/__tests__/**/*.ts',
+        '<rootDir>/src/**/?(*.)+(spec|test).ts'
+    ],
+    transform: {
+        '^.+\\.(ts|js|html)$': [
+            'jest-preset-angular',
+            {
+                tsconfig: '<rootDir>/tsconfig.spec.json',
+                stringifyContentPathRegex: '\\.(html|svg)$',
+            },
+        ],
+    },
+}; 
