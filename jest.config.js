3a1da1e0856e97705da151847329510075f15ef0
aantunes89 <andrepalhares1989@gmail.com>
2025-06-29 21:24:13 +0100

    chore: adding absolute imports in the jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,32 @@
 export default {
-    preset: 'jest-preset-angular',
-    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-        '^src/(.*)$': '<rootDir>/src/$1',
-        '^app/(.*)$': '<rootDir>/src/app/$1',
-        '^assets/(.*)$': '<rootDir>/src/assets/$1',
-        '^environments/(.*)$': '<rootDir>/src/environments/$1',
-    },
-    collectCoverageFrom: [
-        'src/**/*.ts',
-        '!src/**/*.spec.ts',
-        '!src/**/*.d.ts',
-        '!src/main.ts',
-        '!src/polyfills.ts',
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^app/(.*)$': '<rootDir>/src/app/$1',
+    '^assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^environments/(.*)$': '<rootDir>/src/environments/$1',
+    '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.spec.ts',
+    '!src/**/*.d.ts',
+    '!src/main.ts',
+    '!src/polyfills.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts', '<rootDir>/src/**/*.test.ts'],
+  transform: {
+    '^.+\\.(ts|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
     ],
-    coverageDirectory: 'coverage',
-    coverageReporters: ['text', 'lcov', 'html'],
-    testMatch: [
-        '<rootDir>/src/**/*.spec.ts',
-        '<rootDir>/src/**/*.test.ts'
-    ],
-    transform: {
-        '^.+\\.(ts|js|html)$': [
-            'jest-preset-angular',
-            {
-                tsconfig: '<rootDir>/tsconfig.spec.json',
-                stringifyContentPathRegex: '\\.(html|svg)$',
-            },
-        ],
-    },
-}; 
+  },
+};
