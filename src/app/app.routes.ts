bcc104f8844315c124c78383838b17ee5c0c6d3b
aantunes89 <andrepalhares1989@gmail.com>
2025-06-29 11:58:41 +0100

    feat: creating home-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./home-page/home-page.component').then(c => c.HomePageComponent),
+  },
+];
