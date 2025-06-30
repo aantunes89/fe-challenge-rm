56229a3b2819a592af8835e50a31bac97c2223b9
aantunes89 <andrepalhares1989@gmail.com>
2025-06-30 08:32:51 +0100

    feat: loading selected character

--- src/app/character-detail/character-detail.component.ts
+++ src/app/character-detail/character-detail.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import * as CharacterDetailActions from '@app/character-detail/store/character-detail.actions';
 
 @Component({
   selector: 'app-character-detail',
   standalone: true,
   imports: [],
   templateUrl: './character-detail.component.html',
-  styleUrl: './character-detail.component.scss'
+  styleUrl: './character-detail.component.scss',
 })
-export class CharacterDetailComponent {
+export class CharacterDetailComponent implements OnInit {
+  readonly activatedRoute = inject(ActivatedRoute);
+  readonly store = inject(Store);
 
+  ngOnInit(): void {
+    this.activatedRoute.paramMap
+      .pipe(
+        map(params => params.get('id')),
+        filter(id => !!id),
+        tap(characterId => this.loadCharacterDetails(Number(characterId)))
+      )
+      .subscribe();
+  }
+
+  loadCharacterDetails(id: number) {
+    this.store.dispatch(CharacterDetailActions.loadCharacter({ id }));
+  }
 }
