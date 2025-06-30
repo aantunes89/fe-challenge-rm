4b4662bdd9e86c9f187d6e9b0f7bee738abd10c4
aantunes89 <andrepalhares1989@gmail.com>
2025-06-30 19:29:23 +0100

    feat: creating character detail skeleton

--- src/app/character-detail/character-detail.component.ts
+++ src/app/character-detail/character-detail.component.ts
@@ -2,19 +2,27 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { filter, map, tap } from 'rxjs';
 import { Store } from '@ngrx/store';
+import { selectCharacter, selectCharacterLoading, selectCharacterError } from './store';
+import { Observable } from 'rxjs';
+import { Character } from '@app/shared/models/character.interface';
 
 import * as CharacterDetailActions from '@app/character-detail/store/character-detail.actions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-character-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './character-detail.component.html',
   styleUrl: './character-detail.component.scss',
 })
 export class CharacterDetailComponent implements OnInit {
   readonly activatedRoute = inject(ActivatedRoute);
   readonly store = inject(Store);
+
+  character$: Observable<Character | null> = this.store.select(selectCharacter);
+  loading$: Observable<boolean> = this.store.select(selectCharacterLoading);
+  error$: Observable<string | null> = this.store.select(selectCharacterError);
 
   ngOnInit(): void {
     this.activatedRoute.paramMap
